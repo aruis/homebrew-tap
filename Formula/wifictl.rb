bf9bbdd4ed0a92ad6adfedde18c967a1f0dd9c4c
Liu Rui <lovearuis@gmail.com>
2026-03-12 15:12:17 +0800

    Update wifictl formula to 1.26.3

--- Formula/wifictl.rb
+++ Formula/wifictl.rb
@@ -1,17 +1,17 @@
 class Wifictl < Formula
   desc "Manage macOS network service IP and DNS settings"
   homepage "https://github.com/aruis/wifictl"
-  version "1.26.1"
+  version "1.26.3"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/aruis/wifictl/releases/download/v1.26.1/wifictl_v1.26.1_Darwin_arm64.tar.gz"
-      sha256 "deb62dcd157a8e54aacd1a39d25ffe54e1ba5729176fb41355cf2b9daf90cfa6"
+      url "https://github.com/aruis/wifictl/releases/download/v1.26.3/wifictl_v1.26.3_Darwin_arm64.tar.gz"
+      sha256 "dd49714318ed018ce8b5db1ef5464a4e2d2888b25223c484cea9a8e7b257a080"
     end
 
     if Hardware::CPU.intel?
-      url "https://github.com/aruis/wifictl/releases/download/v1.26.1/wifictl_v1.26.1_Darwin_x86_64.tar.gz"
-      sha256 "6f6d50c1208cb660361cb865fbb2d2c1b88d24b7a180f64efe73050fd3a48beb"
+      url "https://github.com/aruis/wifictl/releases/download/v1.26.3/wifictl_v1.26.3_Darwin_x86_64.tar.gz"
+      sha256 "a09fc6a6ec58e70cf3b615d0cc3a5acec2ddb7791448972e2943becfc97d4030"
     end
   end
 
@@ -20,7 +20,7 @@ class Wifictl < Formula
   end
 
   test do
-    output = shell_output("#{bin}/wifictl --help")
-    assert_match "wifictl", output
+    output = shell_output("#{bin}/wifictl version")
+    assert_match version.to_s, output
   end
 end
